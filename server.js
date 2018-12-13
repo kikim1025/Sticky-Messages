9c37c268251a346685a05876d2f5aebf3df66ad2
Ki <kikim1025@gmail.com>
2018-12-12 23:55:23 -0500

    Fixed heroku issue.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('./client/build'));
+  app.use(express.static(path.join(__dirname, './client/build')));
 } else {
   app.use(express.static(path.join(__dirname, './client/public')));
 }
