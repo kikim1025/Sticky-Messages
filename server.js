cfe3ccd1883c561488ec561fc6197d0f671960b4
Ki <kikim1025@gmail.com>
2018-12-13 00:24:47 -0500

    heroku

--- server.js
+++ server.js
@@ -8,19 +8,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
-  console.log("here");
-  app.use(express.static('./client/build'));
+  app.use(express.static(path.join(__dirname, './client/build')));
 } else {
   app.use(express.static(path.join(__dirname, './client/public')));
 }
 app.set('JWTKey', process.env.JWT_KEY);
 
 require('./routes/apiRoutes')(app);
-//require('./routes/htmlRoutes')(app);
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'))
-});
-
+require('./routes/htmlRoutes')(app);
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/sticky-messages_db';
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
